3fd33ba81150154e5e8d29547d1704739788c394
Rayane <rayane.waidi45@gmail.com>
2023-11-23 13:52:51 +0100

    Avancement detection couleur rgb vers hsv

--- _3_TRAITEMENT_d_IMAGES/detectColor.py
+++ _3_TRAITEMENT_d_IMAGES/detectColor.py
@@ -11,24 +11,26 @@ def colorDetection(argv):
 
         #Get image
         #src = cv2.imread(takePhoto.takePhoto())
-        #img = cv2.imread(argv[0])
+        #frame = cv2.imread(argv[0])
+        cv2.imshow("rgb", frame)
 
         #Convert image from original colorspace bgr to hsv
-        hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        cv2.imshow("hsv", hsv_frame)
 
         #Define RGB limits to see
-        """lower_limit_bgr = np.array([0, 0, 0], dtype='uint8')
-        upper_limit_bgr = np.array([255, 255, 255], dtype='uint8')"""
+        lower_limit_rgb = np.array([0, 0, 0], dtype='uint8')
+        upper_limit_rgb = np.array([0, 255, 0], dtype='uint8')
 
-        lower_limit_hsv, upper_limit_hsv = get_limits([255, 0, 0])
+        lower_limit_hsv, upper_limit_hsv = get_limits([0, 255, 0])
         #Convert bgr limits to hsv limits
-        #lowsrc_bgr = np.uint8([[lower_limit_bgr]])
-        #lower_limit_hsv = cv2.cvtColor(lowsrc_bgr, cv2.COLOR_BGR2HSV)
-        #uppsrc_bgr = np.uint8([[upper_limit_bgr]])
-        #upper_limit_hsv = cv2.cvtColor(uppsrc_bgr, cv2.COLOR_BGR2HSV)
+        """lowsrc_rgb = np.uint8([[lower_limit_rgb]])
+        lower_limit_hsv = cv2.cvtColor(lowsrc_rgb, cv2.COLOR_RGB2HSV)
+        uppsrc_rgb = np.uint8([[upper_limit_rgb]])
+        upper_limit_hsv = cv2.cvtColor(uppsrc_rgb, cv2.COLOR_RGB2HSV)"""
 
         #Create mask to only show bgr limits selected
-        mask = cv2.inRange(hsv_img, lower_limit_hsv, upper_limit_hsv)
+        mask = cv2.inRange(frame, lower_limit_hsv, upper_limit_hsv)
 
 
         cv2.imshow("ouput color detection", mask)
@@ -51,8 +53,8 @@ def colorDetection(argv):
     cv2.waitKey(0)"""
 
 def get_limits(color):
-    c = np.uint8([[color]])  # BGR values
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+    c = np.uint8([[color]])  # RGB values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_RGB2HSV)
 
     hue = hsvC[0][0][0]  # Get the hue value
 
