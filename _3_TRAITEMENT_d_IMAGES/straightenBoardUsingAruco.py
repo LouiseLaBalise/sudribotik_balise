475604f633604387420e3f3b58b3a23ce3dc5b2f
Rayane <rayane.waidi45@gmail.com>
2023-12-07 01:47:11 +0100

    Correction de la fonction de redressement. Modif tooltip hsv

--- _3_TRAITEMENT_d_IMAGES/straightenBoardUsingAruco.py
+++ _3_TRAITEMENT_d_IMAGES/straightenBoardUsingAruco.py
@@ -14,7 +14,7 @@ Straighten a board with 4 Aruco tags.
 
 Return function success and new filename.
 """
-def straightenBoardUsingAruco(filename, path="media/", corner_ids = (20, 21, 22, 23), method="CORNER"):
+def straightenBoardUsingAruco(filename, path="media/", corner_ids = (20, 21, 23, 22), method="CORNER"):
 
     #Load image
     image = cv2.imread(filename=path+filename)
@@ -36,27 +36,33 @@ def straightenBoardUsingAruco(filename, path="media/", corner_ids = (20, 21, 22,
     #All 4 Aruco markers are needed to perform image straightening
     corner_ids_no_detected = [] #will store ids
     corners_pos_detected_ids = [] #will store corners positions
-    ids = ids.flatten() #flatten ids array to facilitate its navigation    
+    ids = ids.flatten() #flatten ids array to facilitate its navigation 
+
     for _id in corner_ids: #test to see if they have been detected
         if _id not in ids:
             corner_ids_no_detected.append(_id)
         else :
-            index_of_id = np.where(ids ==_id)[0].item() #cast to int
-            corners_pos_detected_ids.append(corners[index_of_id])
-    
+            index_of_id = np.where(ids ==_id)[0].item() #get the index of the id
+            corners_pos_detected_ids.append(corners[index_of_id]) #add each corners to a list
 
     
     #If not all ids have been detected stop function
     if corner_ids_no_detected:
         print(f"Tag(s) {corner_ids_no_detected} non detecté(s). Impossible de redresser l'image.")
         return False, None
+    
+    #Sort corners pos to have [bottom-left, bottom-right, top-right, top-left]
+    sorted_corners = corners_pos_detected_ids
+    sorted_corners = sorted(sorted_corners, reverse=True, key=lambda coord: coord[0][0][1])#Reverse sort by y axis    
+    sorted_corners[:2] = sorted(sorted_corners[:2], key=lambda coord: coord[0][0][0])#Normal sort by x axis for the bottom    
+    sorted_corners[2:] = sorted(sorted_corners[2:], reverse=True, key=lambda coord: coord[0][0][0])#Reverse sort by x axis for the top
 
-    #Straighten board using center of each Aruco tags
+    #Straighten board using center of each Aruco tags NOT WORKING RIGHT NOW
     if method=="CENTER":
         #Get source points (where points are on the distorded board) 
         src_pts = np.float32([])
 
-        for tag_corners in corners_pos_detected_ids: 
+        for tag_corners in sorted_corners: 
             #1st calc center of each tag
             center_x = int((tag_corners[0][0][0] + tag_corners[0][2][0]) / 2)
             center_y = int((tag_corners[0][0][1] + tag_corners[0][2][1]) / 2)
@@ -86,33 +92,40 @@ def straightenBoardUsingAruco(filename, path="media/", corner_ids = (20, 21, 22,
 
 
     elif method=="CORNER" or method: #default method
-        src_pts = np.float32([])
-        for i,tag_corners in enumerate(corners_pos_detected_ids):             
+        src_pts = np.float32([])            
 
-            #Add each tag outter corner to src points
-            src_pts = np.append(src_pts, [tag_corners[0][i][0], tag_corners[0][i][1]])        
-        
+        #Add each tag outter corner to src points
+        src_pts = np.append(src_pts, sorted_corners[0][0][0])
+        src_pts = np.append(src_pts, sorted_corners[1][0][3])
+        src_pts = np.append(src_pts, sorted_corners[2][0][2])
+        src_pts = np.append(src_pts, sorted_corners[3][0][1])
+
         #Reshape src points to separate each tag
         src_pts = src_pts.reshape(-1, 2)
+        
 
-        #Get max width - src_pts[up or down and left or right][x=0 or y=1]
-        width_up = np.sqrt(((src_pts[0][0] - src_pts[3][0]) ** 2) + ((src_pts[0][1] - src_pts[3][1]) ** 2))
-        width_down = np.sqrt(((src_pts[1][0] - src_pts[2][0]) ** 2) + ((src_pts[1][1] - src_pts[2][1]) ** 2))
-        max_width = max(int(width_up), int(width_down))
+        # Draw the point on the image
+        #cv2.circle(image, np.int32(src_pts[0]), 15, (255, 0, 0), -1)  # -1 fills the circle with color
+        #cv2.circle(image, np.int32(src_pts[1]), 15, (0, 255, 0), -1)  # -1 fills the circle with color
+        #cv2.circle(image, np.int32(src_pts[2]), 15, (0, 0, 255), -1)  # -1 fills the circle with color
+        #cv2.circle(image, np.int32(src_pts[3]), 15, (0, 0, 0), -1)  # -1 fills the circle with color
 
-        #Get max height
-        height_up = np.sqrt(((src_pts[0][0] - src_pts[1][0]) ** 2) + ((src_pts[0][1] - src_pts[1][1]) ** 2))
-        height_down = np.sqrt(((src_pts[2][0] - src_pts[3][0]) ** 2) + ((src_pts[2][1] - src_pts[3][1]) ** 2))
+        #Get max height - src_pts[up or down and left or right][x=0 or y=1]
+        height_up = np.sqrt(((src_pts[0][0] - src_pts[3][0]) ** 2) + ((src_pts[0][1] - src_pts[3][1]) ** 2))
+        height_down = np.sqrt(((src_pts[1][0] - src_pts[2][0]) ** 2) + ((src_pts[1][1] - src_pts[2][1]) ** 2))
         max_height = max(int(height_up), int(height_down))
 
+        #Get max width
+        width_up = np.sqrt(((src_pts[0][0] - src_pts[1][0]) ** 2) + ((src_pts[0][1] - src_pts[1][1]) ** 2))
+        width_down = np.sqrt(((src_pts[2][0] - src_pts[3][0]) ** 2) + ((src_pts[2][1] - src_pts[3][1]) ** 2))
+        max_width = max(int(width_up), int(width_down))
+
         #Get destination points (where src points will be map in the output image) and cast src_pts to float32
-        dst_pts = np.float32([[0, 0],
-                              [0, max_height - 1],
+        dst_pts = np.float32([[0, max_height-1],
                               [max_width - 1, max_height - 1],
-                              [max_width - 1, 0]])
+                              [max_width - 1, 0],
+                              [0, 0]])
         src_pts = np.float32(src_pts)
-
-
 
     #Get matrix transformation
     transform_matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
@@ -166,7 +179,7 @@ if __name__ == "__main__":
     #Get all arguments
     args = parser.parse_args()
     filename = args.path_to_file.split('/')[-1] #get filename
-    corner_ids = args.corner_ids #get corner ids
+    corner_ids = args.ids #get corner ids
     method = args.method #get method
 
     if len(args.path_to_file.split('/')) >=2:   #get path if there is one
