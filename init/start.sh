1c4d8513af83f1b5bd229f7f8ad34e10607a6740
Rayane <rayane.waidi45@gmail.com>
2024-03-11 03:21:40 +0100

    ajout d'un tag conventionel dans le competition launch + modif algorithmique du start.sh

--- init/start.sh
+++ init/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Run competition_start.launch
 
+export BEACON_CAN_COMPETE="false"
 python3 $(pwd)/init/verify_beacon_connection.py
 
-roslaunch beacon_launch competition_start.launch
+if [ "$BEACON_CAN_COMPETE" = "true" ]; then
+    roslaunch beacon_launch competition_start.launch
+fi
